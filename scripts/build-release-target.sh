d96506d0cbc6d54b540ef6ca11a242cda0afc63e
Aaron <20787054+aaronovz1@users.noreply.github.com>
2026-05-20 18:14:50 +0200

    build: fix windows cxx linker selection

--- scripts/build-release-target.sh
+++ scripts/build-release-target.sh
@@ -22,6 +22,7 @@ cgo_cflags="-I${deps_prefix}/include"
 cgo_cxxflags="-I${deps_prefix}/include"
 cgo_ldflags="-L${deps_prefix}/lib"
 extldflags="-lstdc++"
+extld=
 buildmode_args=(-buildmode pie)
 
 case "$target" in
@@ -31,6 +32,7 @@ case "$target" in
     output_name=beatportdl-darwin-arm64
     cc=${CC:-clang}
     cxx=${CXX:-clang++}
+    extld="$cc"
     sdk_path=${MACOS_SDK_PATH:-$(xcrun --sdk macosx --show-sdk-path)}
     export SDKROOT="$sdk_path"
     cgo_ldflags="-F${sdk_path}/System/Library/Frameworks -L${sdk_path}/usr/lib ${cgo_ldflags}"
@@ -41,6 +43,7 @@ case "$target" in
     output_name=beatportdl-darwin-amd64
     cc=${CC:-clang}
     cxx=${CXX:-clang++}
+    extld="$cc"
     sdk_path=${MACOS_SDK_PATH:-$(xcrun --sdk macosx --show-sdk-path)}
     export SDKROOT="$sdk_path"
     cgo_ldflags="-F${sdk_path}/System/Library/Frameworks -L${sdk_path}/usr/lib ${cgo_ldflags}"
@@ -51,9 +54,10 @@ case "$target" in
     output_name=beatportdl-windows-amd64.exe
     cc=${CC:-x86_64-w64-mingw32-gcc}
     cxx=${CXX:-x86_64-w64-mingw32-g++}
+    extld="$cxx"
     cgo_cflags="${cgo_cflags} -DTAGLIB_STATIC -Wall -Wno-deprecated"
     cgo_cxxflags="${cgo_cxxflags} -DTAGLIB_STATIC -Wall -Wno-deprecated"
-    extldflags="-lstdc++ -static-libstdc++ -static-libgcc"
+    extldflags="-static-libstdc++ -static-libgcc"
     buildmode_args=()
     ;;
   *)
@@ -61,6 +65,10 @@ case "$target" in
     exit 1
     ;;
 esac
+
+if [ -z "$extld" ]; then
+  extld="$cc"
+fi
 
 cd "$repo_root"
 go clean -cache
@@ -76,6 +84,6 @@ env \
   CGO_CXXFLAGS="$cgo_cxxflags" \
   CGO_LDFLAGS="$cgo_ldflags" \
   go build "${buildmode_args[@]}" \
-    -ldflags "-w -linkmode external -extldflags '${extldflags}'" \
+    -ldflags "-w -linkmode external -extld=${extld} -extldflags '${extldflags}'" \
     -o "${build_dir}/${output_name}" \
     "$build_src"
